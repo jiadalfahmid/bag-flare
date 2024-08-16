ae2e541a6be3ec7c6ab18184cb5ec5d9571b89bc
jiadalfahmid <jiadalfahmid@gmail.com>
2024-08-17 03:41:18 +0600

    Starting Bag Flare With Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+      bebas: ['Bebas Neue', 'sans-serif'],
+    },
+    extend: {}
+  },
+  content: ['./**/*.{html,js}'],
+}
